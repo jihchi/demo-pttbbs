c2d34ef2221932c869ad54177fd9dd21db343ba2
jihchi <achi@987.tw>
2021-02-22 12:31:11 +0800

    Directly export default function from ReScript compiled JS

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,1 @@
-import IndexRes from "src/PageIndex.js";
-
-// Note:
-// We need to wrap the make call with
-// a Fast-Refresh conform function name,
-// (in this case, uppercased first letter)
-//
-// If you don't do this, your Fast-Refresh will
-// not work!
-export default function Index(props) {
-  return <IndexRes {...props}/>;
-}
+export { default } from 'src/PageIndex.js';
--- pages/trending/boards.js
+++ pages/trending/boards.js
@@ -1,15 +1,1 @@
-import TrendingBoardsRes from "src/PageTrendingBoards.js";
-
-// This can be re-exported as is (no Fast-Refresh issues)
-export { getServerSideProps } from "src/PageTrendingBoards.js";
-
-// Note:
-// We need to wrap the make call with
-// a Fast-Refresh conform function name,
-// (in this case, uppercased first letter)
-//
-// If you don't do this, your Fast-Refresh will
-// not work!
-export default function TrendingBoards(props) {
-  return <TrendingBoardsRes {...props}/>;
-}
+export { default, getServerSideProps } from 'src/PageTrendingBoards.js';
