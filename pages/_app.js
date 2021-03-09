1d8db6bd486e5cc9dddc1115b57a6a1129872d0a
jihchi <achi@987.tw>
2021-03-09 10:09:47 +0800

    Add twind support for SSR

--- pages/_app.js
+++ pages/_app.js
@@ -1,47 +1,8 @@
 import { setup } from 'twind';
+import twindConfig from '../twind.config';
 
 export { default } from 'src/App.js';
 
-setup({
-  theme: {
-    fontSize: {
-      xs: '.75rem',
-      sm: '.875rem',
-      base: '1rem',
-      lg: '1.125rem',
-      xl: '1.25rem',
-      '2xl': '1.5rem',
-      '3xl': '1.875rem',
-      '4xl': '2.25rem',
-      '5xl': '3rem',
-      '6xl': '4rem',
-    },
-    fontFamily: {
-      sans: [
-        '-apple-system',
-        'BlinkMacSystemFont',
-        'Helvetica Neue',
-        'Arial',
-        'sans-serif',
-      ],
-      serif: [
-        'Georgia',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        'Helvetica Neue',
-        'Arial',
-        'sans-serif',
-      ],
-      mono: [
-        'Menlo',
-        'Monaco',
-        'Consolas',
-        'Roboto Mono',
-        'SFMono-Regular',
-        'Segoe UI',
-        'Courier',
-        'monospace',
-      ],
-    },
-  },
-});
+if (typeof window !== 'undefined') {
+  setup(twindConfig);
+}
---
+++ twind.config.js
@@ -0,0 +1,43 @@
+export default {
+  theme: {
+    fontSize: {
+      xs: '.75rem',
+      sm: '.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+    },
+    fontFamily: {
+      sans: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Helvetica Neue',
+        'Arial',
+        'sans-serif',
+      ],
+      serif: [
+        'Georgia',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Helvetica Neue',
+        'Arial',
+        'sans-serif',
+      ],
+      mono: [
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        'Roboto Mono',
+        'SFMono-Regular',
+        'Segoe UI',
+        'Courier',
+        'monospace',
+      ],
+    },
+  },
+};
